2ed3ab3a6bcdbb9f9f5ca078800a40ec0cbc034a
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-05-19 15:50:41 +0200

    feat: load world GeoJSON and render country polygons

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+const GEOJSON_URL = 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
+
 function Map({ onCountryClick }) {
+    const [worldData, setWorldData] = useState(null)
+
+    useEffect(() => {
+        fetch(GEOJSON_URL)
+            .then(res => res.json())
+            .then(data => setWorldData(data))
+    }, [])
+
   return (
     <MapContainer
       center={[20, 0]}
@@ -13,6 +24,18 @@ function Map({ onCountryClick }) {
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; OpenStreetMap &copy; CARTO'
       />
+
+      {worldData && (
+        <GeoJSON
+          data={worldData}
+          style={{
+            fillColor: '#3b5998',
+            fillOpacity: 0.4,
+            color: '#ffffff',
+            weight: 0.5
+          }}
+        />
+      )}
     </MapContainer>
   )
 }
