13653e23f1340a921ec848d06fddf9862c660b11
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-05-19 16:33:15 +0200

    feat: fetch country data from REST Countries API on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Map from './components/Map'
 import Sidebar from './components/Sidebar'
 
 function App() {
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [countryData, setCountryData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (!selectedCountry)  return
+
+    setLoading(true)
+    setCountryData(null)
+
+    fetch('https://restcountries.com/v3.1/alpha/${selectedCountry.code}')
+      .then(res => res.json())
+      .then(data => {
+        setCountryData(data[0])
+        setLoading(false)
+      })
+  }, [selectedCountry])
+
   return (
     <div className="flex h-screen w-screen">
-      <Sidebar country={selectedCountry} />
+      <Sidebar 
+        country={selectedCountry}
+        countryData={countryData}
+        loading={loading}
+      />
       <div className="flex-1">
         <Map onCountryClick={setSelectedCountry} />
       </div>
