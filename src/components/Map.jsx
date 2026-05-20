96f430588cbd7dc52e6c29b99e097b59ebb50423
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-05-20 17:38:39 +0200

    feat: add loading spinner while GeoJSON fetches

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,60 +5,81 @@ import 'leaflet/dist/leaflet.css'
 const GEOJSON_URL = 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
 
 function Map({ onCountryClick }) {
-    const [worldData, setWorldData] = useState(null)
+  const [worldData, setWorldData] = useState(null)
+  const [geoLoading, setGeoLoading] = useState(true)
 
-    useEffect(() => {
-        fetch(GEOJSON_URL)
-            .then(res => res.json())
-            .then(data => setWorldData(data))
-    }, [])
+  useEffect(() => {
+      fetch(GEOJSON_URL)
+          .then(res => res.json())
+          .then(data => {
+              setWorldData(data)
+              setGeoLoading(false)
+          })
+  }, [])
 
-    const onEachCountry = (feature, layer) => {
-        layer.on({
-            mouseover: (e) => {
-                e.target.setStyle({
-                    fillColor: '#ff5722',
-                    fillOpacity: 0.7,
-                })
-            },
-            mouseout: (e) => {
-                e.target.setStyle({
-                    fillColor: '#3b5998',
-                    fillOpacity: 0.4
-                })
-            },
-            click: () => {
-                const countryCode = feature.properties['ISO3166-1-Alpha-2']
-                const countryName = feature.properties.name
-                onCountryClick({ code: countryCode, name: countryName })
-            },
-        })
-    }
-    return (
-    <MapContainer
-      center={[20, 0]}
-      zoom={2}
-      className="h-full w-full"
-      style={{ background: '#1a1a2e' }}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; OpenStreetMap &copy; CARTO'
-      />
+  const onEachCountry = (feature, layer) => {
+      layer.on({
+          mouseover: (e) => {
+              e.target.setStyle({
+                  fillOpacity: 0.85,
+              })
+          },
+          mouseout: (e) => {
+              e.target.setStyle({
+                  fillOpacity: 0.6
+              })
+          },
+          click: () => {
+              const countryCode = feature.properties['ISO3166-1-Alpha-2']
+              const countryName = feature.properties.name
+              onCountryClick({ code: countryCode, name: countryName })
+          },
+      })
+  }
 
-      {worldData && (
-        <GeoJSON
-          data={worldData}
-          style={{
-            fillColor: '#3b5998',
-            fillOpacity: 0.4,
-            color: '#ffffff',
-            weight: 0.5
-          }}
-            onEachFeature={onEachCountry}
-        />
+  const getcountryStyle = (feature) => {
+      return {
+          fillColor: '#3b5998',
+          fillOpacity: 0.6,
+          color: '#ffffff',
+          weight: 0.5
+      }
+  }
+
+  return (
+
+    <div className="relative h-full w-full">
+      {geoLoading && (
+        <div className="absolute inset-0 flex z-10 items-center justify-center bg-gray-950"> 
+          <div className="flex flex-col items-center gap-3"> 
+            <div className="w-8 h-8 border-2 border-blue-500 border-t-transparent rounded-full animate-spin">
+              <p className="text-gray-400 text-sm">Loading world data...</p>
+            </div>
+          </div>
+        </div>
       )}
-    </MapContainer>
+
+      <MapContainer
+            center={[20, 0]}
+            zoom={2}
+            className="h-full w-full"
+            style={{ background: '#1a1a2e' }}
+          >
+            <TileLayer
+              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; OpenStreetMap &copy; CARTO'
+            />
+
+            {worldData && (
+              <GeoJSON
+                key="default"
+                data={worldData}
+                style={getcountryStyle}
+                onEachFeature={onEachCountry}
+              />
+            )}
+        </MapContainer>
+    </div>
   )
 }
 
