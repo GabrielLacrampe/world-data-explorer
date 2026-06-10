17e1d2c465e683158bc1781f9b5552652fe0611d
Gabriel <45939703+GabrielLacrampe@users.noreply.github.com>
2026-06-10 16:02:46 +0200

    feat: switch base map tiles from Stadia to ESRI Physical Map

--- src/utils/mapStyles.js
+++ src/utils/mapStyles.js
@@ -1,24 +1,30 @@
 export const MAP_STYLE = {
   version: 8,
+  glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
   sources: {
     'stadia-tiles': {
       type: 'raster',
-      tiles: ['https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'],
+      tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}'],
       tileSize: 256,
-      attribution: '© Stadia Maps © OpenMapTiles © OpenStreetMap contributors',
+      attribution: '© Esri, US National Park Service',
     },
   },
   layers: [
     {
       id: 'background',
       type: 'background',
-      paint: { 'background-color': '#0a0e1a' },
+      paint: { 'background-color': '#b8d4e8' },
     },
     {
       id: 'stadia-tiles',
       type: 'raster',
       source: 'stadia-tiles',
-      paint: { 'raster-opacity': 0.7 },
+      paint: {
+        'raster-opacity': 1.0,
+        'raster-saturation': 0.4,
+        'raster-brightness-max': 0.38,  // 1.0 = original, bajar oscurece
+        'raster-contrast': 0.45,        // sube contraste para que los colores sigan vivos
+      },
     },
   ],
 }
